8818221c97922f572e4bce3d8cd9f7074e2bb7b1
Manikandan L <128012731+manikandanmk7305@users.noreply.github.com>
2024-04-27 23:26:44 +0530

    Array1.java

--- Array1.java
+++ Array1.java
@@ -4,10 +4,9 @@ You are playing the PUBG game and you entered into the Bootcamp. There you viewe
 you have to choose the biggest gun in the drop box. Find the biggest gun and you will get the chicken dinner. Input Format: 
 Input consists of n+1 integers. The first integer corresponds to ‘n’ , the size of the array. The next ‘n’ integers correspond 
 to the elements in the array. Assume that the maximum value of n is 15. Output Format: Refer sample output for details.
-Example 1:
+Example:
 
 Input: 
-
  
 5
 
